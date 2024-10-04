4af0bae87970811882240e3ed85f1967bea20e26
mrskg0438 <gaudshwetkamal0438@gmail.com>
2024-10-05 00:25:44 +0530

    rearrangement

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -40,42 +40,42 @@ successful projects on time and within budget.`
         {
             id: 1,
             title: "Programming Languages",
-            items: ["JavaScript, ", "TypeScript, ", "Python, ", "Java, ", "C/C++ "]
+            items: "JavaScript, TypeScript, Python, Java, C/C++ "
         },
         {
             id: 2,
             title: 'Libraries & Frameworks',
-            items: ["React, ", " React Native, ", "NextJs, ", "Redux, ", "Redux-Saga "]
+            items: "React, React Native, NextJs, Redux, Redux-Saga "
         },
         {
             id: 3,
             title: 'Frontend CSS Library',
-            items: ["HTML/CSS,", "SASS, ", "SCSS, ", " Material-UI, ", "Tailwind CSS, ", "Bootstrap, ", "Native Base, "]
+            items: "HTML/CSS, SASS, SCSS, Material-UI, Tailwind CSS, Bootstrap, Native Base, "
         },
         {
             id: 4,
             title: 'Other Skills & Tools',
-            items: ["Git/Github, ", " VS Code, ", "Android Studio, ", "Adobe Photoshop, ", "Adobe Illustrator "]
+            items: "Git/Github, VS Code, Android Studio, Adobe Photoshop, Adobe Illustrator "
         },
         {
             id: 5,
             title: 'Cloud/Databases',
-            items: ["MongoDB(Exploring), ", "PostgreSQL(Exploring) "]
+            items: "MongoDB(Exploring), PostgreSQL(Exploring) "
         },
         {
             id: 7,
             title: 'Areas of Interest',
-            items: ["Web Design and Development, ", " Mobile App Development, ", "Vulnerability Assessment & Penetration Testing "]
+            items: "Web Design and Development,  Mobile App Development, Vulnerability Assessment & Penetration Testing"
         },
         {
             id: 8,
             title: 'Soft Skills',
-            items: ["Problem Solving, ", "Self-learning, ", "Adaptability "]
+            items: "Problem Solving, Self-learning, Adaptability "
         },
         {
             id: 6,
             title: 'Communication Languages',
-            items: ["Hindi, ", " English, ", " Gujarati"]
+            items: "Hindi, English, Gujarati"
         }
     ],
     funFact: [
@@ -171,7 +171,7 @@ const AboutPage = () => {
             // </Stack>
         )
     }
-    const SkillsCard = ({ title, skills }: { title: string, skills: string[] }) => {
+    const SkillsCard = ({ title, skills }: { title: string, skills: string }) => {
         return (
             <div
                 style={{
@@ -217,21 +217,20 @@ const AboutPage = () => {
 
                     }}
                 >
-                    {skills?.map((skill, index) => {
-                        return (
-                            <li
-                                key={index}
-                                style={{
-                                    listStyle: 'none',
-                                    fontSize: 14,
-                                    fontWeight: 500,
-                                    fontFamily: 'FiraCode',
-                                    color: isDark ? textLight : textGray,
-                                }}
 
-                            >{skill}</li>
-                        )
-                    })}
+
+                    <li
+
+                        style={{
+                            listStyle: 'none',
+                            fontSize: 14,
+                            fontWeight: 500,
+                            fontFamily: 'FiraCode',
+                            color: isDark ? textLight : textGray,
+                        }}
+
+                    >{skills}</li>
+
 
                 </ul>
 
