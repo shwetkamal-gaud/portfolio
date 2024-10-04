0ca95bae8c980770bdc2d745eb64dd43a1735a61
mrskg0438 <gaudshwetkamal0438@gmail.com>
2024-10-04 22:23:47 +0530

    fixed

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -6,8 +6,47 @@ import { LogoImg, SunIcon } from '@/assets/svg'
 import Typography from './Typography'
 import Link from 'next/link'
 import DropDown from './DropDown'
-import SideBar from './SideBar'
 
+import { EmailIcon, GithubIcon, TelegramIcon } from "../assets/svg";
+
+const sidebarData = [
+    {
+        name: 'discord',
+        icon: <TelegramIcon />,
+        link: 'https://t.me/mrskg0438',
+        id: 1
+    },
+    {
+        name: 'github',
+        icon: <GithubIcon />,
+        link: "https://github.com/mrskg0438",
+        id: 2
+    },
+    {
+        name: 'email',
+        icon: <EmailIcon />,
+        link: "mailto: gaudshwetkamal0438@gmail.com",
+        id: 3
+    }
+]
+
+
+const SideBar = () => {
+    return (
+        <div className="d-flex align-items-center justify-content-center  w-100 pt-4 ">
+            <div className="d-flex flex-column  align-items-center gap-2" style={{ width: '35%' }}>
+                <div className="d-none d-lg-block" style={{ display: "block", width: '2px', height: 240, backgroundColor: '#ABB2BF' }}></div>
+                <div className="d-flex flex-lg-column flex-row">
+                    {
+                        sidebarData.map((item) => (
+                            <a className="sidebar" key={item.id} href={item.link} rel="noreferrer" target="_blank">{item.icon}</a>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const navbarData = {
     routes: [
