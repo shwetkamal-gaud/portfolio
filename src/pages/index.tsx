c61c2c92008610939ee227dfa587c2cbd54f6433
mrskg0438 <gaudshwetkamal0438@gmail.com>
2024-10-15 10:41:21 +0530

    image changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,7 +106,6 @@ export default function Home() {
                   <Image className="img-fluid"
                     alt={"hero_img"}
                     src={image}
-                    style={{ width: '22rem', height: '38rem' }}
                   />
                   <h2>Shwetkamal Gaud</h2>
                 </div>
